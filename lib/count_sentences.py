88d9d65d22c5b8c9adde7b3da6a9eeba4e5bdc2f
BilRos <billyrossowuor@gmail.com>
2024-06-05 08:19:08 +0300

    Counting sentences was abit tricky

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,34 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+
+    def __init__ (self,value = ""):
+        self.value=value
+      
+    @property
+    def control_value(self):
+        return self._value
+
+    @control_value.setter
+    def control_value(self, control_value):
+        if isinstance(control_value,str):
+            self._value=control_value
+        else:
+            print ('The value must be a string.')
+
+
+    def is_sentence(self):
+        return True if self.value.endswith (".") else False
+
+    def is_question():
+        return True if self.value.endswith ("?") else False
+
+    def is_exaclamation():
+        return True if self.value.endswith ("!") else False
+
+    def count_sentences():
+        sentences = re.split(r'[.!?]', self.value)
+
+        sentences = [value for value in sentences if value.strip()]
+        return len(sentences)
